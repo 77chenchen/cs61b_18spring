9b11d51dbe6014a4e105b8f21dc92f2fafe0fb98
wangqichen <wangqichen517@gmail.com>
2025-02-20 17:53:52 +0800

    finish

--- lab2/intnode/src/slllist.java
+++ lab2/intnode/src/slllist.java
@@ -3,13 +3,42 @@ public class slllist{
     public slllist(int x){
         first = new intnode(x,null);
     }
-    public static void main(String[] args){
-        slllist L = new slllist(15);
-    }
     public void addFirst(int x){
         first = new intnode(x,first);
     }
     public int getFirst(){
         return first.item;
+    }
+    public void addLast(int x){
+        intnode p = first;
+        while(p.next!=null)
+        {
+            p=p.next;
+        }
+        p.next = new intnode(x,null);
+
+    }
+    public int one_size(){
+        intnode p = first;
+        int number=0;
+        while(p != null){
+            number+=1;
+            p=p.next;
+        }
+        return number;
+
+    }
+    private static int size(intnode p){
+        if(p.next==null){
+            return 1;
+        }
+        return 1+ size(p.next);
+    }
+    public int size(){
+        return size(first);
+    }
+
+    public static void main(String[] args){
+        slllist L = new slllist(15);
     }
 }
